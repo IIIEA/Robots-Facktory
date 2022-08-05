74b9001f311294bc6499d7bd738333f859b26827
IIIEA <egor.solo2000@bk.ru>
2022-08-05 12:50:11 +0300

    Add craft anim

--- Assets/Scripts/CraftZone/CraftTable.cs
+++ Assets/Scripts/CraftZone/CraftTable.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CraftTable : MonoBehaviour
 {
     [SerializeField] private PartsPlacer _partsPlacer;
+    [SerializeField] private Transform _craftPosition;
+
+    private RobotPart[] _partsToCraft;
 
     public event Action<bool> PartsOnTablceChanged;
     public event Action<int> CraftLvlCalculated;
@@ -22,12 +26,12 @@ public class CraftTable : MonoBehaviour
 
     private void OnPartsCountChanged()
     {
-        var parts = GetComponentsInChildren<RobotPart>();
+        _partsToCraft = GetComponentsInChildren<RobotPart>();
         int lvl = 0;
 
-        for (int i = 0; i < parts.Length; i++)
+        for (int i = 0; i < _partsToCraft.Length; i++)
         {
-            lvl += parts[i].Lvl;
+            lvl += _partsToCraft[i].Lvl;
         }
 
         CraftLvlCalculated?.Invoke(lvl);
@@ -37,6 +41,17 @@ public class CraftTable : MonoBehaviour
 
     public void Craft()
     {
+        if (_partsToCraft.Length <= 0)
+            return;
 
+
+        foreach (var part in _partsToCraft)
+        {
+            Sequence sequence = DOTween.Sequence();
+
+            sequence.Append(part.transform.DOMoveY(_craftPosition.position.y, 0.3f).SetEase(Ease.InOutBack));
+            sequence.Append(part.transform.DOMoveX(_craftPosition.position.x, 0.3f).SetEase(Ease.InBack));
+            sequence.Append(part.transform.DOScale(0, 0.3f));
+        }
     }
 }
